660d03210925fd5be9dd3e0b55818e1ec58f19a3
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-26 19:50:26 -0300

    Configurando application

---
+++ NeohConcessionaria.Application/ApplicationModule.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NeohConcessionaria.Infra.Persistence;
+using NeohConcessionaria.Infra.Repositories.Concessionarias;
+using NeohConcessionaria.Infra.Repositories.Veiculos;
+using NeohConcessionaria.Infra.Repositories.Vendas;
+using NeohConcessionaria.Infra.Repositories;
+using NeohConcessionaria.Infrastructure.Repositories.Fabricantes;
+using NeohConcessionaria.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeohConcessionaria.Application.Services.FabricanteServices;
+using NeohConcessionaria.Application.Services.ConcessionariaService;
+using NeohConcessionaria.Application.Services.ConcessionariaServices;
+
+namespace NeohConcessionaria.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddServices();
+            return services;
+        }
+
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFabricanteService, FabricanteService>();
+            services.AddScoped<IConcessionariaService, ConcessionariaService>();
+            return services;
+        }
+    }
+}
--- NeohConcessionaria.MVC/Program.cs
+++ NeohConcessionaria.MVC/Program.cs
@@ -1,10 +1,13 @@
 using NeohConcessionaria.Infrastructure;
+using NeohConcessionaria.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddInfrasctructure(builder.Configuration);
+builder.Services.AddApplication().AddInfrasctructure(builder.Configuration);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
